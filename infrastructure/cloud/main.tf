1b127bd9551765255bc85e0c07c88cd329d6004e
Tim Davis <contact@timdav.is>
2024-05-19 17:14:08 -0700

    ssl certificates in cloud emulation

--- infrastructure/cloud/main.tf
+++ infrastructure/cloud/main.tf
@@ -15,25 +15,35 @@ resource "docker_container" "cloud" {
       ]
     ])
   }
+  ports {
+    internal = 80
+    external = 80
+    ip       = "127.0.10.1"
+  }
+  ports {
+    internal = 443
+    external = 443
+    ip       = "127.0.10.1"
+  }
   upload {
     file = "/usr/local/etc/haproxy/haproxy.cfg"
     content = templatefile("${path.module}/templates/haproxy.tftpl.cfg", {
 
     })
   }
-  # upload {
-  #   file = "/usr/local/etc/haproxy/hosts.map"
-  #   content = templatefile("${path.module}/templates/hosts.tftpl.map", {
-  #     hosts = flatten([
-  #       for service, hosts in var.aliases : [
-  #         for host in hosts : {
-  #           name    = host.hostname
-  #           service = service
-  #         }
-  #       ]
-  #     ])
-  #   })
-  # }
+  upload {
+    file = "/usr/local/etc/haproxy/hosts.map"
+    content = templatefile("${path.module}/templates/hosts.tftpl.map", {
+      hosts = flatten([
+        for service, hosts in var.aliases : [
+          for host in hosts : {
+            name    = host.hostname
+            service = service
+          }
+        ]
+      ])
+    })
+  }
   # upload {
   #   file = "/usr/local/etc/haproxy/ports.map"
   #   content = templatefile("${path.module}/templates/ports.tftpl.map", {
@@ -50,6 +60,6 @@ resource "docker_container" "cloud" {
   volumes {
     read_only      = true
     host_path      = abspath("${path.module}/../.tmp/tls.pem")
-    container_path = "/usr/local/etc/ssh/certs/tls.pem"
+    container_path = "/usr/local/etc/ssl/certs/tls.pem"
   }
 }
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -59,44 +59,44 @@ module "cloud" {
   }
 }
 
-module "nodes" {
-  source = "./nodes"
-  cluster = {
-    name          = module.conf.cluster_name
-    k8s_version   = module.conf.versions.k8s
-    talos_version = module.conf.versions.talos
-  }
-  dns = {
-    private_ip = module.dns.private_ip
-  }
-  net = {
-    bridge_network_id  = module.net.bridge_network_id
-    private_network_id = module.net.private_network_id
-    ctrl_cidr          = module.conf.net.ctrl_cidr
-    work_cidr          = module.conf.net.work_cidr
-    pod_cidr           = module.conf.net.pod_cidr
-    service_cidr       = module.conf.net.service_cidr
-  }
-  cmd  = module.conf.nodes.cmd
-  ctrl = module.conf.nodes.ctrl
-  work = module.conf.nodes.work
-  cilium = {
-    version = module.conf.versions.cilium
-  }
-  kubelet_serving_cert_approver = {
-    version = module.conf.versions.kubelet_serving_cert_approver
-  }
-  local_path_provisioner = {
-    version = module.conf.versions.local_path_provisioner
-  }
-}
+# module "nodes" {
+#   source = "./nodes"
+#   cluster = {
+#     name          = module.conf.cluster_name
+#     k8s_version   = module.conf.versions.k8s
+#     talos_version = module.conf.versions.talos
+#   }
+#   dns = {
+#     private_ip = module.dns.private_ip
+#   }
+#   net = {
+#     bridge_network_id  = module.net.bridge_network_id
+#     private_network_id = module.net.private_network_id
+#     ctrl_cidr          = module.conf.net.ctrl_cidr
+#     work_cidr          = module.conf.net.work_cidr
+#     pod_cidr           = module.conf.net.pod_cidr
+#     service_cidr       = module.conf.net.service_cidr
+#   }
+#   cmd  = module.conf.nodes.cmd
+#   ctrl = module.conf.nodes.ctrl
+#   work = module.conf.nodes.work
+#   cilium = {
+#     version = module.conf.versions.cilium
+#   }
+#   kubelet_serving_cert_approver = {
+#     version = module.conf.versions.kubelet_serving_cert_approver
+#   }
+#   local_path_provisioner = {
+#     version = module.conf.versions.local_path_provisioner
+#   }
+# }
 
-resource "local_sensitive_file" "kubeconfig" {
-  content  = module.nodes.kubeconfig
-  filename = "${path.module}/.kube/config"
-}
+# resource "local_sensitive_file" "kubeconfig" {
+#   content  = module.nodes.kubeconfig
+#   filename = "${path.module}/.kube/config"
+# }
 
-resource "local_sensitive_file" "talosconfig" {
-  content  = module.nodes.talosconfig
-  filename = "${path.module}/.talos/config"
-}
+# resource "local_sensitive_file" "talosconfig" {
+#   content  = module.nodes.talosconfig
+#   filename = "${path.module}/.talos/config"
+# }
